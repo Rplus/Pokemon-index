1aea03385e272be69dda3f0f064cfdeb145aa65d
Rplus <rplus.tw@gmail.com>
2019-10-17 19:03:51 +0800

    Updates

---
+++ precache-manifest.ab30f2add61cde1596f942aba988adf6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcdd7664d3ce9480e509",
+    "url": "/Pokemon-index/css/app.e69dba4d.css"
+  },
+  {
+    "revision": "02cc18c0b85f1a4615d7e5075b619e25",
+    "url": "/Pokemon-index/index.html"
+  },
+  {
+    "revision": "4f856d76107074fe96a5",
+    "url": "/Pokemon-index/js/about.882e4c31.js"
+  },
+  {
+    "revision": "bcdd7664d3ce9480e509",
+    "url": "/Pokemon-index/js/app.8da8cd80.js"
+  },
+  {
+    "revision": "4c25284eb3dc71132911",
+    "url": "/Pokemon-index/js/chunk-vendors.63062326.js"
+  },
+  {
+    "revision": "5cb7ce1c9d59b333d59d0faa2e5c8fa6",
+    "url": "/Pokemon-index/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Pokemon-index/robots.txt"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/Pokemon-index/src/assets/logo_82b9c.png"
+  }
+]);
