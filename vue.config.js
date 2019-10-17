4a3d24e61f81ec99efcf20546d8b3e5f4daa744c
Rplus <rplus.tw@gmail.com>
2019-10-17 19:06:47 +0800

    add deploy task

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Pokemon-index/'
+    : '/Pokemon-index/',
+  outputDir: 'Pokemon-index',
+  productionSourceMap: false,
+  chainWebpack: config => {
+    config.module
+      .rule('images')
+      .test(/\.(png|jpe?g|gif|webp)(\?.*)?$/)
+      .use('url-loader')
+      .loader('url-loader')
+      .options({ limit: -1, name: '[path][name]_[hash:5].[ext]' });
+  },
+};
